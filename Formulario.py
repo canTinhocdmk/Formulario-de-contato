52235ac7df1b5831eb8e7c52cc5219cc3a23f6f0
canTinhocdmk <52702785+canTinhocdmk@users.noreply.github.com>
2022-01-17 09:33:46 -0300

    Add files via upload

---
+++ Formulario.py
@@ -0,0 +1,8 @@
+nome = input ('Nome')
+cpf = input ('CPF')
+endereço = input ('Endereço')
+idade = input ('Idade')
+altura = input ('Altura')
+telefone = input ('Telefone')
+
+print(nome, 'De CPF', cpf, 'Residente No Endereço', endereço, 'Possui o Telefone:', telefone, 'Com', idade, 'anos e', altura, 'Metros')
